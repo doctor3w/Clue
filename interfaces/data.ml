9f3697e3dfd3a092174b02b52c5a33026b5975c6
Drew Dunne <drew@drewsdunne.com>
2016-11-07 22:02:44 -0500

    changed few things from meetings

--- interfaces/data.ml
+++ interfaces/data.ml
@@ -36,18 +36,3 @@ type player = {id: string;
                agent: agent}
 
 type game = {players: player list; curr_player: string; board: loc}
-
-(* [import_board] takes in a filename of a game configuration file and
- * converts the file into a usable game model for stepping through. *)
-val import_board : string -> model
-
-(* [get_move_options] gets the options of Roll and Passage that the current
- * player can make. *)
-val get_move_options : model -> move list
-
-(* [get_movement_options] gets the options of the locations that the current
- * player can move to. These options also come with a description in one of
- * the following fashions:
- *        head towars [room name]
- *        go into [room name] *)
-val get_movement_options : model -> (string * loc) list
