e7f294109222d63fe7227d095d9f290789b95970
Ubuntu <ubuntu@csvm>
2016-11-07 22:23:34 -0500

    small fix

--- interfaces/view.mli
+++ interfaces/view.mli
@@ -1,18 +1,18 @@
 open Data
 
 (* Prompts the user for a file so that it can be imported into the Model *)
-val prompt_filename : () -> string
+val prompt_filename : unit -> string
 
 (* Prints a description of who's turn it is. *)
-val print_turn : game -> ()
+val print_turn : game -> unit
 
 (* Prompts the user for whether he rolls dice or not. *)
 val prompt_move : move list -> string
 
 (* Prints a description of whether the player elected to Roll or Passage. *)
-val print_move : move -> ()
+val print_move : move -> unit
 
 (* Prompts the user for his. *)
 val prompt_movement : (string * loc) list -> string
 
-val print_movement : (string * loc) -> ()
+val print_movement : (string * loc) -> unit
