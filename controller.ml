a35413d72de1d281801f76a9711dfe583f4e1d85
Drew Dunne <drew@drewsdunne.com>
2016-11-15 15:12:30 -0500

    changes

--- controller.ml
+++ controller.ml
@@ -18,19 +18,19 @@ let get_cur_next plrs cur =
 
 (* [step] Recursively progresses through the game by doing one agent turn
  * at a time. *)
-let step game =
+let rec step game =
   let (curr_p, next_p) = get_cur_next game.players game.curr_player in
   let () = Display.display_turn game in
   let Who = match curr_p.agent with
             | DumbAI_t -> DumbAI
-            | _ -> in
+            | _ -> DumbAI in
   let move = Who.answer_move (Model.get_move_options game) in
   let () = Display.display_move move in
   match move with
   | RollDice -> begin
     let movement_opt = Model.get_movement_options ((Random.int 11) + 2) in
     let movement = Who.get_movement movement_opt in
-    let curr_p' = {curr_p with curr_location = movement}
+    let curr_p' = {curr_p with curr_location = movement} in
     match movement with
     | Room(s, l) when s = "envelope" ->
     | _ -> (* Get Guess *)
