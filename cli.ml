673d974e75298e88221e71878f6f5a7dd3bf5992
Drew Dunne <drew@drewsdunne.com>
2016-11-15 18:16:46 -0500

    more controller

--- cli.ml
+++ cli.ml
@@ -1,44 +1,45 @@
 open Data
+open Agent
 
 (* Displays the provided error message. *)
-val display_error : string -> unit
+let display_error = failwith ""
 
 (* Displays a description of who's turn it is. *)
-val display_turn : game -> unit
+let display_turn = failwith ""
 
 (* Prompts the user for a file so that it can be imported into the Model *)
-val prompt_filename : unit -> string
+let prompt_filename = failwith ""
 
 (* Prompts the user for whether he rolls dice or not. *)
-val prompt_move : move list -> string
+let prompt_move = failwith ""
 
 (* Displays a description of whether the agent elected to Roll or Passage. *)
-val display_move : move -> unit
+let display_move = failwith ""
 
 (* Prompts the user for his. *)
-val prompt_movement : (string * loc) list -> string
+let prompt_movement = failwith ""
 
 (* Displays the movement the agen took on his turn *)
-val display_movement : (string * loc) -> unit
+let display_movement = failwith ""
 
 (* Prompts the user for a guess.
  * Takes in the current location (must be a room) and
  * a bool which says whether or not it is the final accusation.
  * returns a string of the user's response. *)
-val prompt_guess : loc -> bool -> string
+let prompt_guess = failwith ""
 
 (* Displays a guess (by the user or AI). *)
-val display_guess : guess -> unit
+let display_guess = failwith ""
 
 (* Prompts the user for a card to show.
  * Can be any card from the provided hand, and must be in the guess.
  * Can be none if there is no card to show. *)
-val prompt_answer : hand -> guess -> string
+let prompt_answer = failwith ""
 
 (* Displays the card shown to the human agent and by whom.
  * If None, the user is told just told who showed a card, but not the details
  * of the card. *)
-val display_answer : card option -> string -> unit
+let display_answer = failwith ""
 
 (* Displays end game victory text. *)
-val display_victory : unit -> unit
+let display_victory = failwith ""
