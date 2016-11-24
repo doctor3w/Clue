50a841337e3c8c05c14b8075b7cf5cd99f0de30d
Drew Dunne <drew@drewsdunne.com>
2016-11-23 23:40:24 -0500

    started debug

--- main.ml
+++ main.ml
@@ -2,6 +2,6 @@
  * Main starts up the game using the passed in CLI argument as the game config.
  *)
 let () =
-  let () = print_string "Initializing..." in
+  let () = print_string "Initializing...\n" in
   let file_name = try Some Sys.argv.(1) with Invalid_argument _ -> None in
   Controller.start file_name
